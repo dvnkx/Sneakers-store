73bc49b9af705729bb18370cd00f656134483748
Bogdan <dvnkxxxx@gmail.com>
2023-06-01 20:56:17 +0300

    fix deleted prev input

--- client/src/components/ui/index.js
+++ client/src/components/ui/index.js
@@ -2,7 +2,7 @@ export { default as ImagesModel } from "./CardsComponents/ImagesModel.jsx";
 export { default as Card } from "./CardsComponents/Card/Card.jsx";
 export { default as CardInfo } from "./CardsComponents/CardInfo.jsx";
 export { default as CardSkeleton } from "./CardsComponents/CardSkeleton.jsx";
-export { default as Input } from "./FilterInput/FilterInput.jsx";
 export { default as Spinner } from "./Spinner/Spinner.jsx";
 export { default as BackLine } from "./BackLine.jsx";
 export { default as ImgIntro } from "./ImgIntro.jsx";
+export { default as Input } from "./Input/Input.jsx";
